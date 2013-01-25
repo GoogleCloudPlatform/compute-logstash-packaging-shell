6f897864b8f1d66b828130e337f044b80dcf2e8a
Jon Skarpeteig <lesspublic@gmail.com>
2013-01-25 15:24:15 +0100

    Fixed issue with config files getting overwritten on upgrade

--- README.md
+++ README.md
@@ -14,5 +14,5 @@ file. Alternatively, you can do it manually:
 
 4) run the following command:
 
-    fpm -n logstash -v 1.1.9 -d default-jre -a all -C logstash-packaging -m "<lesspublic@gmail.com>" --pre-install logstash-packaging/logstash.preinstall --description "Logstash Open Source Log Management" --url 'http://www.logstash.net/' -t deb -s dir etc usr var
+    fpm -n logstash -v 1.1.9 -d default-jre -a all -C logstash-packaging -m "<lesspublic@gmail.com>" --pre-install logstash-packaging/logstash.preinstall --description "Logstash Open Source Log Management" --url 'http://www.logstash.net/' -t deb --config-files etc/logstash/syslog.conf --config-files etc/default/logstash -s dir etc usr var
 
--- package.sh
+++ package.sh
@@ -4,4 +4,4 @@ cd ..
 git clone git://github.com/Yuav/logstash-packaging.git
 wget http://logstash.objects.dreamhost.com/release/logstash-${VERSION}-monolithic.jar -O logstash-packaging/usr/share/logstash/logstash.jar
 #wget https://logstash.objects.dreamhost.com/release/logstash-1.1.8-flatjar.jar
-fpm -n logstash -v $VERSION -d default-jre -a all -C logstash-packaging -m "<lesspublic@gmail.com>" --pre-install logstash-packaging/logstash.preinstall --description "Logstash Open Source Log Management" --url 'http://www.logstash.net/' -t deb -s dir etc usr var
+fpm -n logstash -v $VERSION -d default-jre -a all -C logstash-packaging -m "<lesspublic@gmail.com>" --pre-install logstash-packaging/logstash.preinstall --description "Logstash Open Source Log Management" --url 'http://www.logstash.net/' -t deb --config-files etc/logstash/syslog.conf --config-files etc/default/logstash -s dir etc usr var
