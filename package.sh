c13df82f3661cee903cfc2be31437b8c0905cb8e
Rodrigo De Castro <rdc@google.com>
2013-06-28 14:30:05 -0700

    Fix directory used to download logstash.jar to allow for directories other than logstash-packaging

--- package.sh
+++ package.sh
@@ -3,6 +3,6 @@ VERSION=1.1.10
 CURRENT_DIR=$PWD
 cd ..
 #git clone git://github.com/Yuav/logstash-packaging.git --depth=1
-wget http://logstash.objects.dreamhost.com/release/logstash-${VERSION}-monolithic.jar -O logstash-packaging/usr/share/logstash/logstash.jar
+wget http://logstash.objects.dreamhost.com/release/logstash-${VERSION}-monolithic.jar -O $CURRENT_DIR/usr/share/logstash/logstash.jar
 #wget https://logstash.objects.dreamhost.com/release/logstash-${VERSION}-flatjar.jar
 fpm -n logstash -v $VERSION -a all -C $CURRENT_DIR -m "<lesspublic@gmail.com>" --pre-install logstash-packaging/logstash.preinstall --description "Logstash Open Source Log Management" --url 'http://www.logstash.net/' -t deb --config-files etc/logstash/syslog.conf --config-files etc/default/logstash -s dir etc usr var
