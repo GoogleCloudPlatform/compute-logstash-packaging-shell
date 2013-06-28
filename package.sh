9ee1dd10645bd4eb7a6d4ec93db6994f1da64bb0
Rodrigo De Castro <rdc@google.com>
2013-06-28 15:15:17 -0700

    Add missed reference to 'logstash-packaging' that had to be replaced by $CURRENT_DIR

--- package.sh
+++ package.sh
@@ -5,4 +5,4 @@ cd ..
 #git clone git://github.com/Yuav/logstash-packaging.git --depth=1
 wget http://logstash.objects.dreamhost.com/release/logstash-${VERSION}-monolithic.jar -O $CURRENT_DIR/usr/share/logstash/logstash.jar
 #wget https://logstash.objects.dreamhost.com/release/logstash-${VERSION}-flatjar.jar
-fpm -n logstash -v $VERSION -a all -C $CURRENT_DIR -m "<lesspublic@gmail.com>" --pre-install logstash-packaging/logstash.preinstall --description "Logstash Open Source Log Management" --url 'http://www.logstash.net/' -t deb --config-files etc/logstash/syslog.conf --config-files etc/default/logstash -s dir etc usr var
+fpm -n logstash -v $VERSION -a all -C $CURRENT_DIR -m "<lesspublic@gmail.com>" --pre-install $CURRENT_DIR/logstash.preinstall --description "Logstash Open Source Log Management" --url 'http://www.logstash.net/' -t deb --config-files etc/logstash/syslog.conf --config-files etc/default/logstash -s dir etc usr var
