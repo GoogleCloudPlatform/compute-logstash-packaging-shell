acb7212cbdbac9fc1d95fe4a3b6186749353e887
Rodrigo De Castro <rdc@google.com>
2013-06-28 16:04:20 -0700

    Add conflicts information so both server and shipper are not installed at the same time.

--- package-shipper.sh
+++ package-shipper.sh
@@ -11,5 +11,4 @@ if [ ! -f "$JAR_FILE" ]; then
 fi
 
 #wget https://logstash.objects.dreamhost.com/release/logstash-${VERSION}-flatjar.jar
-echo `pwd`
-fpm -n logstash-shipper -v $VERSION -a all -C $CURRENT_DIR -m "<lesspublic@gmail.com>" --pre-install $CURRENT_DIR/logstash.preinstall --post-install $CURRENT_DIR/logstash.postinstall  --description "Logstash Open Source Log Management - Log Shipper" --url 'http://www.logstash.net/' -t deb --config-files etc/logstash/syslog-shipper.conf --config-files etc/default/logstash-shipper -s dir etc/default/logstash-shipper etc/init.d etc/logstash/syslog-shipper.conf usr/share/logstash/logstash.jar var
+fpm -n logstash-shipper -v $VERSION -a all -C $CURRENT_DIR -m "<lesspublic@gmail.com>" --conflicts logstash-server --pre-install $CURRENT_DIR/logstash.preinstall --post-install $CURRENT_DIR/logstash.postinstall  --description "Logstash Open Source Log Management - Log Shipper" --url 'http://www.logstash.net/' -t deb --config-files etc/logstash/syslog-shipper.conf --config-files etc/default/logstash-shipper -s dir etc/default/logstash-shipper etc/init.d etc/logstash/syslog-shipper.conf usr/share/logstash/logstash.jar var
