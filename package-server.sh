bdbc33e133150bd64957e4ae93a7bfa8ee3aa814
Rodrigo De Castro <rdc@google.com>
2013-06-28 15:51:23 -0700

    Add postinstall script to fix file permissions. Verify if logstash is already downloaded instead of downloading it everytime. Call out which files to include, to allow for packages for multiple packages to be present in the directories.

--- package-server.sh
+++ package-server.sh
@@ -3,6 +3,12 @@ VERSION=1.1.10
 CURRENT_DIR=$PWD
 cd ..
 #git clone git://github.com/Yuav/logstash-packaging.git --depth=1
-wget http://logstash.objects.dreamhost.com/release/logstash-${VERSION}-monolithic.jar -O $CURRENT_DIR/usr/share/logstash/logstash.jar
+
+JAR_FILE=$CURRENT_DIR/usr/share/logstash/logstash.jar
+
+if [ ! -f "$JAR_FILE" ]; then
+  wget http://logstash.objects.dreamhost.com/release/logstash-${VERSION}-monolithic.jar -O $JAR_FILE  
+fi
+
 #wget https://logstash.objects.dreamhost.com/release/logstash-${VERSION}-flatjar.jar
-fpm -n logstash -v $VERSION -a all -C $CURRENT_DIR -m "<lesspublic@gmail.com>" --pre-install $CURRENT_DIR/logstash.preinstall --description "Logstash Open Source Log Management" --url 'http://www.logstash.net/' -t deb --config-files etc/logstash/syslog.conf --config-files etc/default/logstash -s dir etc usr var
+fpm -n logstash -v $VERSION -a all -C $CURRENT_DIR -m "<lesspublic@gmail.com>" --pre-install $CURRENT_DIR/logstash.preinstall --post-install $CURRENT_DIR/logstash.postinstall --description "Logstash Open Source Log Management" --url 'http://www.logstash.net/' -t deb --config-files etc/logstash/syslog.conf --config-files etc/default/logstash -s dir etc/default/logstash etc/init.d etc/logstash/syslog.conf usr/share/logstash/logstash.jar var
